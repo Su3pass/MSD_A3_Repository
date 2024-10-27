f507c654f703d1ad276c6e526a370f8b8c827912
Gao2024-sys <3081828167@qq.com>
2024-10-27 22:53:01 +0800

    Create the Logout Servlet
    As a new user, I want to register an account through a simple interface, so that I can logout the system.

---
+++ Create the Logout Servlet .java
@@ -0,0 +1,27 @@
+java
+
+	import javax.servlet.*;  
+
+	import javax.servlet.http.*;  
+
+	import java.io.*;  
+
+	  
+
+	public class LogoutServlet extends HttpServlet {  
+
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+
+	        HttpSession session = request.getSession(false);  
+
+	        if (session != null) {  
+
+	            session.invalidate();  
+
+	        }  
+
+	        response.sendRedirect("login.jsp");  
+
+	    }  
+
+	}
